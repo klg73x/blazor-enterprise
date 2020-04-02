74281b5b4f9395e407e783151361c2cc74f60e72
Kevin Gagnon <keving@kultera.com>
2020-04-02 13:59:37 -0400

    Added extra component for grid

--- BethanysPieShopHRM.UI/Startup.cs
+++ BethanysPieShopHRM.UI/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BethanysPieShopHRM.UI.Services;
 using BethanysPieShopHRM.UI.Data;
+using Blazor.FlexGrid;
+using BethanysPieShopHRM.UI.Pages;
 
 namespace BethanysPieShopHRM.UI
 {
@@ -40,6 +42,9 @@ namespace BethanysPieShopHRM.UI
             services.AddTransient<ISurveyDataService, SurveyDataService>();
             services.AddTransient<ICurrencyDataService, CurrencyDataService>();
             services.AddTransient<IExpenseApprovalService, ManagerApprovalService>();
+            services.AddFlexGridServerSide(cfg => {
+                cfg.ApplyConfiguration(new ExpenseGridConfiguration());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
